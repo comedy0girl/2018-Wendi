375322126ccbcd307edc7597bd231dec108c2cc2
switchstancezackeaton <zack.eaton@switchstanceit.com>
2018-09-14 16:36:24 +0100

    home page stuff

--- includes/_upcoming.php
+++ includes/_upcoming.php
@@ -1,4 +1,7 @@
 <div class="twelve columns upcoming-eps">
+	<div  class="large-behind-text">
+			<img class="bg-text" src="<?php bloginfo('template_url') ?>/assets/images/goldbergs.png">
+		</div>
 	
 	<div class="wow animated slideInRight offset-by-one nine columns upcoming-inner" data-wow-duration="2s" >
 	<!-- <?php
--- single.php
+++ single.php
@@ -10,9 +10,9 @@
 				preg_match('/<img.*(src)="([^"]*)"/i', $featured, $matches);
 				$thumb = $matches[2]; ?>
 
-			<div class="six columns post-side-image" style="background-image:url('<?php echo $thumb; ?>') ">
-			</div>
-			<div class="six columns single-post-text">
+
+			<div class="offset-by-one nine columns single-post-text" >
+				<div class="post-image" style="background-image:url('<?php echo $thumb; ?>') "></div>
 				<h1><?php the_title(); ?></h1>
 				<p class="tags"><?php the_time('F') ?> <?php the_time('j') ?> <?php the_time('Y')?> / <?php	
 					$categories = get_the_category();
