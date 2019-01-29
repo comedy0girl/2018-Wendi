0929491398b7a2baa8c2c58a914d1007bcb910fc
comedy0girl <comedy.writer.ash@gmail.com>
2019-01-29 19:13:51 +0000

    follow links on main page

--- includes/_follow-wendi.php
+++ includes/_follow-wendi.php
@@ -1,5 +1,5 @@
 <div class="row twelve columns image-links">
-		<div  class="large-behind-text">
+		<!-- <div  class="large-behind-text">
 			<img class="bg-text" src="<?php bloginfo('template_url') ?>/assets/images/queen.png">
 		</div>
 		<div class="twelve columns the-links">
@@ -22,5 +22,51 @@
 					<h3>On Instagram</h3>
 				</div>
 			</div>
-		</div>
+		</div> -->
+
+	<div class="twelve columns the-links">
+
+			<div  class="large-behind-text">
+				<img class="bg-text" src="<?php bloginfo('template_url') ?>/assets/images/queen.png">
+			</div>
+			<h3 class="row section-title">Follow Wendi</h3>
+			<div class="inner-links-boxes">
+				<a href="/biography/">
+					<div class="four columns images" style="background-image: url('<?php bloginfo('template_url') ?>/assets/images/bio.jpg');">
+						<div class="links-inner">
+							<p>Bio</p>
+							<h3>About Wendi</h3>
+						</div>
+					</div>
+				</a>
+				<a href="https://twitter.com/wendimclendonco">
+					<div class="four columns images" style="background-image: url('<?php bloginfo('template_url') ?>/assets/images/insta.jpg');">
+						<div class="links-inner">
+							<p>Follow Wendi</p>
+							<h3>On Twitter</h3>
+						</div>
+					</div>
+				</a>
+				<a href="https://www.instagram.com/wendi_mclendon_covey/">
+					<div class="four columns images" style="background-image: url('<?php bloginfo('template_url') ?>/assets/images/twitter.jpg');">
+						<div class="links-inner">
+							<p>Follow Wendi</p>
+							<h3>On Instagram</h3>
+						</div>
+					</div>
+				</a>
+			</div>
+
+
+
+
+
+
+
+
+
+
+
 	</div>
+
+</div>
