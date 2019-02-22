ebb1a92df2180dd80b8906c487690ff5a97bb0d0
comedy0girl <comedy.writer.ash@gmail.com>
2019-02-22 01:15:42 +0000

    back to top moved to side and main menu z-index

--- footer.php
+++ footer.php
@@ -27,9 +27,9 @@
 			wp_nav_menu(['theme_location' => 'footer-menu' ]); ?>
 		</div>
         <div class="twelve columns copyright">
-        	<p> Designed and Developed by <a href="https://twitter.com/wendifansite">@wendifansite</a>. 	<a href="#" class="classic-button-white back-to-top">Back To Top</a></p>
+        	<p> Designed and Developed by <a href="https://twitter.com/wendifansite">@wendifansite</a>. 	
         	<div class="top-button">
-        	
+        		<a href="#" class="classic-button-white back-to-top">Back To Top</a></p>
         	</div>
         </div>
         <?php wp_footer(); ?>
